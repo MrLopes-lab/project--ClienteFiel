0b16583aa75c3d87c8cb9ec4611ac7efae713ae6
MrDev <lopesrodrigues05@gmail.com>
2020-08-27 21:25:53 -0300

    🔥 Landing

--- appclientefiel/src/pages/Landing/index.tsx
+++ appclientefiel/src/pages/Landing/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import { RectButton } from 'react-native-gesture-handler';
+
+import { Container, Banner, Title } from './styles';
+
+import landingImg from '../../assets/images/Landing.png';
+import boxIcon from '../../assets/images/icons/Box.png';
+import starIcon from '../../assets/images/icons/Star.png';
 
 const Landing: React.FC = () => {
   return (
-    <View>
-      <Text>Bem-vindo Marcelo</Text>
-    </View>
+    <>
+      <Banner source={landingImg} />
+      <Container>
+        <Title>
+          Seja bem-vindo,
+          {'\n'}
+          <Title>O que deseja fazer?</Title>
+        </Title>
+
+        <View>
+          <RectButton>
+            <Image source={boxIcon} />
+            <Text>Clientes</Text>
+          </RectButton>
+
+          <RectButton>
+            <Image source={starIcon} />
+            <Text>Novo Cliente</Text>
+          </RectButton>
+        </View>
+      </Container>
+    </>
   );
 };
 
---
+++ appclientefiel/src/pages/Landing/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #333533;
+  justify-content: center;
+  padding: 40px;
+`;
+
+export const Banner = styled.Image`
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  font-family: 'PressStart2P-Regular';
+  color: #fff;
+  font-size: 14px;
+  line-height: 30px;
+  margin-top: 80px;
+`;
